ec4b201007a726cf62540c79a70d2aad1c79b4f1
KonstantinS007 <uzerovalex9@gmail.com>
2023-01-20 22:01:50 +0300

    modul 14

--- script.py
+++ script.py
@@ -775,13 +775,13 @@ n = 3
 # print('Сумма к оплате: ',n,'руб.')
 
 # Proverka chujogo koda
-money = float(input('Ввести сумму:'))
-per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-deposit = []
-deposit.append(money*5.6/100)
-deposit.append(money*5.9/100)
-deposit.append(money*4.28/100)
-deposit.append(money*4/100)
-print(deposit)
-
-print('Максимальная сумма, которую вы можете заработать', max(deposit))
+# money = float(input('Ввести сумму:'))
+# per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+# deposit = []
+# deposit.append(money*5.6/100)
+# deposit.append(money*5.9/100)
+# deposit.append(money*4.28/100)
+# deposit.append(money*4/100)
+# print(deposit)
+#
+# print('Максимальная сумма, которую вы можете заработать', max(deposit))
