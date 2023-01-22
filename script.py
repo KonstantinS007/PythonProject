a763f25b97158691269caead2b4bcf1f0c2ed91e
KonstantinS007 <uzerovalex9@gmail.com>
2023-01-22 23:29:33 +0300

    modul 14

--- script.py
+++ script.py
@@ -1100,7 +1100,7 @@ n = 3
 # # 0
 
 
-import time
+# import time
 
 
 # def decorator_time(fn):
@@ -1328,3 +1328,21 @@ import time
 
 # a = ["asd", "bbd", "ddfa", "mcsa"]
 # print(list(map(lambda x: len(x), a)))
+
+
+#  Задание 12.7.3 (HW-03)
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+money = int(input("Сумма: "))
+
+# map(function, list) позволят выполнить определённые действия для каждого элемента списка
+# ().__mul__ умножение элементов, получения списка значений полученых накоплений в каждом банке
+# deposit = list(map(int, list(map((money/100).__mul__, per_cent.values()))))
+
+deposit = list(map(lambda x: int(x*money/100), per_cent.values()))
+
+# это в следующем модуле100
+# deposit = [int(v * money / 100) for v in per_cent.values()]
+
+# находим максимальное значение и его индекс в списке
+i = deposit.index(max(deposit))
+print(deposit[i])
