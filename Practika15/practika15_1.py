aa08764827ab220fa275104d7f217800fc874730
KonstantinS007 <uzerovalex9@gmail.com>
2023-01-27 21:23:55 +0300

    PEP8

--- Practika15/practika15_1.py
+++ Practika15/practika15_1.py
@@ -1,4 +1,4 @@
-def change_Text(text):
+def change_text(text):
     """
     Функция принимает строку с текстом.
     Убирает все знаки препинания и возвращает
@@ -11,15 +11,14 @@ def change_Text(text):
     return text.split()
 
 
-
-def most_Common(text, length=0):
+def most_common(text, length=0):
     """
     Функция принимает список и ограничение по длине.
     Возвращает самый часто встречающийся элемент.
     Если есть несколько элементов с одинаковой самой большой частотой, то вернёт их все.
     """
 
-    most_common = []
+    most_common1 = []
     qty_most_common = 0
 
     for item in text:
@@ -27,39 +26,39 @@ def most_Common(text, length=0):
             qty = text.count(item)
             if qty > qty_most_common and qty > 2:
                 qty_most_common = qty
-                most_common = [item]
+                most_common1 = [item]
             elif qty == qty_most_common:
-                most_common.append(item)
+                most_common1.append(item)
 
-    return list(set(most_common))
+    return list(set(most_common1))
 
 
-def most_Length(text):
+def most_length(text):
     """
     Функция принимает список.
     Возвращает самый длинный элемент.
     Если есть несколько элементов с одинаковой самой большой длиной, то вернёт их все.
     """
 
-    most_length = []
+    most_length1 = []
     qty_most_length = 0
     alphabet = r'abcdefghijklmnopqrstuvwxyz`ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     for item in text:
         for char in item:
             if char not in alphabet:
-                charEn = False
+                char_en = False
             else:
-                charEn = True
+                char_en = True
 
-            if charEn:
+            if char_en:
                 qty = len(item)
                 if qty > qty_most_length:
                     qty_most_length = qty
-                    most_length = [item]
+                    most_length1 = [item]
                 elif qty == qty_most_length:
-                    most_length.append(item)
+                    most_length1.append(item)
 
-    return list(set(most_length))
+    return list(set(most_length1))
 
 
 nameFile = input('Название файла ' + '(text1.txt): ')
@@ -67,8 +66,7 @@ nameFile = input('Название файла ' + '(text1.txt): ')
 with open(nameFile, encoding='utf8') as f:
     fileText = f.read()
 
-
-fileText = change_Text(fileText)
+fileText = change_text(fileText)
 print(len(fileText))
-print(f'Список самых частых слов длинной более трёх символов: {most_Common(fileText, 3)}')
-print(f'Список самых длинных английских слов: {most_Length(fileText)}')
+print(f'Список самых частых слов длинной более трёх символов: {most_common(fileText, 3)}')
+print(f'Список самых длинных английских слов: {most_length(fileText)}')
