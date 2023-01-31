93a5830344e9d09411007a5b4c69500e3c8fba89
KonstantinS007 <uzerovalex9@gmail.com>
2023-02-01 00:34:18 +0300

    practice_17_derevo

--- Task_17/script.py
+++ Task_17/script.py
@@ -30,7 +30,7 @@
 #
 # print(par_checker('(5+6)*(7+8)/(4+3)'))
 
-pars = {")": "(", "]": "["}
+# pars = {")": "(", "]": "["}
 
 
 # def par_checker_mod(string):
@@ -48,76 +48,147 @@ pars = {")": "(", "]": "["}
 #
 # print(par_checker('(5+6)*(7+8)/(4+3)'))
 
-N_max = int(input("Определите размер очереди:"))
+# N_max = int(input("Определите размер очереди:"))
+#
+# queue = [0 for _ in range(N_max)]  # инициализируем список с нулевыми элементами
+# order = 0  # будем хранить сквозной номер задачи
+# head = 0  # указатель на начало очереди
+# tail = 0  # указатель на элемент следующий за концом очереди
+#
+# def is_empty():
+#     return head == tail and queue[head] == 0
+#
+#
+# def size():
+#     if is_empty():
+#         return 0
+#     elif head == tail:
+#         return N_max
+#     elif head > tail:
+#         return N_max - head + tail
+#     else:
+#         return tail - head
+#
+# def add():
+#     global tail, order
+#     order += 1
+#     queue[tail] = order
+#     print("Задача №%d добавлена" % (queue[tail]))
+#
+#     tail = (tail + 1) % N_max
+#
+#
+# def show():
+#     print("Задача №%d в приоритете" % (queue[head]))
+#
+#
+# def do():
+#     global head
+#     print("Задача №%d выполнена" % (queue[head]))
+#     queue[head] = 0
+#     head = (head + 1) % N_max
+#
+#
+# while True:
+#     cmd = input("Введите команду:")
+#     if cmd == "empty":
+#         if is_empty():
+#             print("Очередь пустая")
+#         else:
+#             print("В очереди есть задачи")
+#     elif cmd == "size":
+#         print("Количество задач в очереди:", size())
+#     elif cmd == "add":
+#         if size() != N_max:
+#             add()
+#         else:
+#             print("Очередь переполнена")
+#     elif cmd == "show":
+#         if is_empty():
+#             print("Очередь пустая")
+#         else:
+#             show()
+#     elif cmd == "do":
+#         if is_empty():
+#             print("Очередь пустая")
+#         else:
+#             do()
+#     elif cmd == "exit":
+#         for _ in range(size()):
+#             do()
+#         print("Очередь пустая. Завершение работы")
+#         break
+#     else:
+#         print("Введена неверная команда")
 
-queue = [0 for _ in range(N_max)]  # инициализируем список с нулевыми элементами
-order = 0  # будем хранить сквозной номер задачи
-head = 0  # указатель на начало очереди
-tail = 0  # указатель на элемент следующий за концом очереди
+# task 17_5_5
 
-def is_empty():
-    return head == tail and queue[head] == 0
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left_child = None
+        self.right_child = None
 
-
-def size():
-    if is_empty():
-        return 0
-    elif head == tail:
-        return N_max
-    elif head > tail:
-        return N_max - head + tail
-    else:
-        return tail - head
-
-def add():
-    global tail, order
-    order += 1
-    queue[tail] = order
-    print("Задача №%d добавлена" % (queue[tail]))
-
-    tail = (tail + 1) % N_max
-
-
-def show():
-    print("Задача №%d в приоритете" % (queue[head]))
-
-
-def do():
-    global head
-    print("Задача №%d выполнена" % (queue[head]))
-    queue[head] = 0
-    head = (head + 1) % N_max
-
-
-while True:
-    cmd = input("Введите команду:")
-    if cmd == "empty":
-        if is_empty():
-            print("Очередь пустая")
+    def insert_left(self, next_value):
+        if self.left_child is None:
+            self.left_child = BinaryTree(next_value)
         else:
-            print("В очереди есть задачи")
-    elif cmd == "size":
-        print("Количество задач в очереди:", size())
-    elif cmd == "add":
-        if size() != N_max:
-            add()
+            new_child = BinaryTree(next_value)
+            new_child.left_child = self.left_child
+            self.left_child = new_child
+        return self
+
+    def insert_right(self, next_value):
+        if self.right_child is None:
+            self.right_child = BinaryTree(next_value)
         else:
-            print("Очередь переполнена")
-    elif cmd == "show":
-        if is_empty():
-            print("Очередь пустая")
-        else:
-            show()
-    elif cmd == "do":
-        if is_empty():
-            print("Очередь пустая")
-        else:
-            do()
-    elif cmd == "exit":
-        for _ in range(size()):
-            do()
-        print("Очередь пустая. Завершение работы")
-        break
-    else:
-        print("Введена неверная команда")
+            new_child = BinaryTree(next_value)
+            new_child.right_child = self.right_child
+            self.right_child = new_child
+        return self
+
+    def pre_order(self):
+        print(self.value)  # процедура обработки
+
+        if self.left_child is not None:  # если левый потомок существует
+            self.left_child.pre_order()  # рекурсивно вызываем функцию
+
+        if self.right_child is not None:  # если правый потомок существует
+            self.right_child.pre_order()  # рекурсивно вызываем функцию
+
+    def post_order(self):
+        if self.left_child is not None:  # если левый потомок существует
+            self.left_child.post_order()  # рекурсивно вызываем функцию
+
+        if self.right_child is not None:  # если правый потомок существует
+            self.right_child.post_order()  # рекурсивно вызываем функцию
+
+        print(self.value)  # процедура обработки
+
+    def in_order(self):
+        if self.left_child is not None:  # если левый потомок существует
+            self.left_child.in_order()  # рекурсивно вызываем функцию
+
+        print(self.value)  # процедура обработки
+
+        if self.right_child is not None:  # если правый потомок существует
+            self.right_child.in_order()  # рекурсивно вызываем функцию
+
+
+node_root = BinaryTree(2).insert_left(7).insert_right(5)
+# левое поддерево корня /2|7|6\
+node_7 = node_root.left_child.insert_left(2).insert_right(6)
+# правое поддерево предыдущего узла /5|6|11\
+node_6 = node_7.right_child.insert_left(5).insert_right(11)
+# правое поддерево корня /|5|9\
+node_5 = node_root.right_child.insert_right(9)
+# левое поддерево предыдущего узла корня /4|9|\
+node_9 = node_5.right_child.insert_left(4)
+
+node_root.pre_order()
+print()
+node_root.post_order()
+print()
+node_root.in_order()
+
 
