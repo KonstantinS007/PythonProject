86d4b219a082ba2910b13ae24873b0a178ec6577
KonstantinS007 <uzerovalex9@gmail.com>
2023-02-01 15:31:20 +0300

    task_17.8.4

--- Task_17/script.py
+++ Task_17/script.py
@@ -123,72 +123,320 @@
 
 # task 17_5_5
 
-class BinaryTree:
-    def __init__(self, value):
-        self.value = value
-        self.left_child = None
-        self.right_child = None
+# class BinaryTree:
+#     def __init__(self, value):
+#         self.value = value
+#         self.left_child = None
+#         self.right_child = None
+#
+#     def insert_left(self, next_value):
+#         if self.left_child is None:
+#             self.left_child = BinaryTree(next_value)
+#         else:
+#             new_child = BinaryTree(next_value)
+#             new_child.left_child = self.left_child
+#             self.left_child = new_child
+#         return self
+#
+#     def insert_right(self, next_value):
+#         if self.right_child is None:
+#             self.right_child = BinaryTree(next_value)
+#         else:
+#             new_child = BinaryTree(next_value)
+#             new_child.right_child = self.right_child
+#             self.right_child = new_child
+#         return self
+#
+#     def pre_order(self):
+#         print(self.value)  # процедура обработки
+#
+#         if self.left_child is not None:  # если левый потомок существует
+#             self.left_child.pre_order()  # рекурсивно вызываем функцию
+#
+#         if self.right_child is not None:  # если правый потомок существует
+#             self.right_child.pre_order()  # рекурсивно вызываем функцию
+#
+#     def post_order(self):
+#         if self.left_child is not None:  # если левый потомок существует
+#             self.left_child.post_order()  # рекурсивно вызываем функцию
+#
+#         if self.right_child is not None:  # если правый потомок существует
+#             self.right_child.post_order()  # рекурсивно вызываем функцию
+#
+#         print(self.value)  # процедура обработки
+#
+#     def in_order(self):
+#         if self.left_child is not None:  # если левый потомок существует
+#             self.left_child.in_order()  # рекурсивно вызываем функцию
+#
+#         print(self.value)  # процедура обработки
+#
+#         if self.right_child is not None:  # если правый потомок существует
+#             self.right_child.in_order()  # рекурсивно вызываем функцию
+#
+#
+# node_root = BinaryTree(2).insert_left(7).insert_right(5)
+# # левое поддерево корня /2|7|6\
+# node_7 = node_root.left_child.insert_left(2).insert_right(6)
+# # правое поддерево предыдущего узла /5|6|11\
+# node_6 = node_7.right_child.insert_left(5).insert_right(11)
+# # правое поддерево корня /|5|9\
+# node_5 = node_root.right_child.insert_right(9)
+# # левое поддерево предыдущего узла корня /4|9|\
+# node_9 = node_5.right_child.insert_left(4)
+#
+# node_root.pre_order()
+# print()
+# node_root.post_order()
+# print()
+# node_root.in_order()
 
-    def insert_left(self, next_value):
-        if self.left_child is None:
-            self.left_child = BinaryTree(next_value)
-        else:
-            new_child = BinaryTree(next_value)
-            new_child.left_child = self.left_child
-            self.left_child = new_child
-        return self
 
-    def insert_right(self, next_value):
-        if self.right_child is None:
-            self.right_child = BinaryTree(next_value)
-        else:
-            new_child = BinaryTree(next_value)
-            new_child.right_child = self.right_child
-            self.right_child = new_child
-        return self
+# Task 17_6_1
 
-    def pre_order(self):
-        print(self.value)  # процедура обработки
+# class Node:  # класс элемента
+#     def __init__(self, value=None, next_=None):  # инициализируем
+#         self.value = value  # значением
+#         self.next = next_  # и ссылкой на следующий элемент
+#
+#     def __str__(self):
+#         return "Node value = " + str(self.value)
+#
+#     def __iter__(self):  # объявляем класс как итератор
+#         self.current = self.first  # в текущий элемент помещаем первый
+#         return self  # возвращаем итератор
+#
+#     def __next__(self):  # метод перехода
+#         if self.current is None:  # если текущий стал последним
+#             raise StopIteration  # вызываем исключение
+#         else:
+#             node = self.current  # сохраняем текущий элемент
+#             self.current = self.current.next  # совершаем переход
+#             return node  # и возвращаем сохраненный
+#
+#     def __len__(self):
+#         count = 0
+#         pointer = self.first
+#         while pointer is not None:
+#             count += 1
+#             pointer = pointer.next
+#         return count
+#
+#
+# class LinkedList:  # класс списка
+#     def __init__(self):  # инициализируем пустым
+#         self.first = None
+#         self.last = None
+#
+#     def clear(self):  # очищаем список
+#         self.__init__()
+#
+#     def __str__(self):  # функция печати
+#         R = ''
+#         pointer = self.first  # берем первый указатель
+#         while pointer is not None:  # пока указатель не станет None
+#             R += str(pointer.value)  # добавляем значение в строку
+#             pointer = pointer.next  # идем дальше по указателю
+#             if pointer is not None:  # если он существует добавляем пробел
+#                 R += ' '
+#         return R
+#
+#     def pushleft(self, value):
+#         if self.first is None:
+#             self.first = Node(value)
+#             self.last = self.first
+#         else:
+#             new_node = Node(value, self.first)
+#             self.first = new_node
+#
+#     def pushright(self, value):
+#         if self.first is None:
+#             self.first = Node(value)
+#             self.last = self.first
+#         else:
+#             new_node = Node(value)
+#             self.last.next = new_node
+#             self.last = new_node
+#
+#     def popleft(self):
+#         if self.first is None:  # если список пустой, возвращаем None
+#             return None
+#         elif self.first == self.last:  # если список содержит только один элемент
+#             node = self.first  # сохраняем его
+#             self.__init__()  # очищаем
+#             return node  # и возвращаем сохраненный элемент
+#         else:
+#             node = self.first  # сохраняем первый элемент
+#             self.first = self.first.next  # меняем указатель на первый элемент
+#             return node  # возвращаем сохраненный
+#
+#     def popright(self):
+#         if self.first is None:  # если список пустой, возвращаем None
+#             return None
+#         elif self.first == self.last:  # если список содержит только один элемент
+#             node = self.first  # сохраняем его
+#             self.__init__()  # очищаем
+#             return node  # и возвращаем сохраненный элемент
+#         else:
+#             node = self.last  # сохраняем последний
+#             pointer = self.first  # создаем указатель
+#             while pointer.next is not node:  # пока не найдем предпоследний
+#                 pointer = pointer.next
+#             pointer.next = None  # обнуляем указатели, чтобы
+#             self.last = pointer  # предпоследний стал последним
+#             return node  # возвращаем сохраненный
+#
+#     def __iter__(self):  # объявляем класс как итератор
+#         self.current = self.first  # в текущий элемент помещаем первый
+#         return self  # возвращаем итератор
+#
+#     def __next__(self):  # метод перехода
+#         if self.current is None:  # если текущий стал последним
+#             raise StopIteration  # вызываем исключение
+#         else:
+#             node = self.current  # сохраняем текущий элемент
+#             self.current = self.current.next  # совершаем переход
+#             return node  # и возвращаем сохраненный
+#
+#     def __len__(self):
+#         count = 0
+#         pointer = self.first
+#         while pointer is not None:
+#             count += 1
+#             pointer = pointer.next
+#         return count
+#
+#
+# LL = LinkedList()
+#
+# LL.pushright(1)
+# LL.pushleft(2)
+# LL.pushright(3)
+# LL.popright()
+# LL.pushleft(4)
+# LL.pushright(5)
+# LL.popleft()
+#
+# print(LL)
+# print(len(LL))
 
-        if self.left_child is not None:  # если левый потомок существует
-            self.left_child.pre_order()  # рекурсивно вызываем функцию
+# Task_17_7
 
-        if self.right_child is not None:  # если правый потомок существует
-            self.right_child.pre_order()  # рекурсивно вызываем функцию
+# def find(array, element):
+#     for i, a in enumerate(array):
+#         if a == element:
+#             return i
+#     return False
+#
+#
+# def count(array, element):
+#     count = 0
+#     for a in array:
+#         if a == element:
+#             count += 1
+#     return count
+#
+# array = list(map(int, input().split()))
+# element = int(input())
+#
+# print(find(array, element))
+# print(count(array, element))
+# 5 5 5 6 7 - input
+# 6         - input
+# 3         - print
+# 1         - count
 
-    def post_order(self):
-        if self.left_child is not None:  # если левый потомок существует
-            self.left_child.post_order()  # рекурсивно вызываем функцию
+# def binary_search(array, element, left, right):
+#     if left > right:  # если левая граница превысила правую,
+#         return False  # значит элемент отсутствует
+#
+#     middle = (right + left) // 2  # находимо середину
+#     if array[middle] == element:  # если элемент в середине,
+#         return middle  # возвращаем этот индекс
+#     elif element < array[middle]:  # если элемент меньше элемента в середине
+#         # рекурсивно ищем в левой половине
+#         return binary_search(array, element, left, middle - 1)
+#     else:  # иначе в правой
+#         return binary_search(array, element, middle + 1, right)
+#
+#
+# element = int(input())
+# array = [i for i in range(1, 100)]  # 1,2,3,4,...
+#
+# # запускаем алгоритм на левой и правой границе
+# print(binary_search(array, element, 0, 99))
 
-        if self.right_child is not None:  # если правый потомок существует
-            self.right_child.post_order()  # рекурсивно вызываем функцию
+# Task 17_8
 
-        print(self.value)  # процедура обработки
+# import random  # модуль, с помощью которого перемешиваем массив
+#
+# # пусть имеем массив всего лишь из 9 элементов
+# array = [2, 3, 1, 4, 6, 5, 9, 8, 7]
+#
+# is_sort = False  # станет True, если отсортирован
+# count = 0  # счетчик количества перестановок
+#
+# while not is_sort:  # пока не отсортирован
+#     count += 1  # прибавляем 1 к счётчику
+#
+#     random.shuffle(array)  # перемешиваем массив
+#
+#     # проверяем, отсортирован ли
+#     is_sort = True
+#     for i in range(len(array)-1):
+#         if array[i] > array[i + 1]:
+#             is_sort = False
+#             break
+#
+# print(array)
+# # [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# print(count)
+# # 290698
 
-    def in_order(self):
-        if self.left_child is not None:  # если левый потомок существует
-            self.left_child.in_order()  # рекурсивно вызываем функцию
+# 17.8.1
 
-        print(self.value)  # процедура обработки
+# array = 100
+# count = 1
+# for i in range(1, array + 1):
+#     count *= i
+#
+# print(len(str(count)))
+# # 158!!!
 
-        if self.right_child is not None:  # если правый потомок существует
-            self.right_child.in_order()  # рекурсивно вызываем функцию
+# array = [2, 3, 1, 4, 6, 5, 9, 8, 7]
+# count = 0
+# for i in range(len(array)):  # проходим по всему массиву
+#     idx_min = i  # сохраняем индекс предположительно минимального элемента
+#     for j in range(i, len(array)):
+#         count += 1
+#         if array[j] < array[idx_min]:
+#             idx_min = j
+#
+#     if i != idx_min:  # если индекс не совпадает с минимальным, меняем
+#         array[i], array[idx_min] = array[idx_min], array[i]
 
+# for i in range(len(array)):
+#     idx_max = i
+#     for j in range(i, len(array)):
+#         count += 1
+#         if array[j] > array[idx_max]:
+#             idx_max = j
+#     if i != idx_max:
+#         array[i], array[idx_max] = array[idx_max], array[i]
+#
+# print(array)
+# print(count)
 
-node_root = BinaryTree(2).insert_left(7).insert_right(5)
-# левое поддерево корня /2|7|6\
-node_7 = node_root.left_child.insert_left(2).insert_right(6)
-# правое поддерево предыдущего узла /5|6|11\
-node_6 = node_7.right_child.insert_left(5).insert_right(11)
-# правое поддерево корня /|5|9\
-node_5 = node_root.right_child.insert_right(9)
-# левое поддерево предыдущего узла корня /4|9|\
-node_9 = node_5.right_child.insert_left(4)
+# Task 17.8.3
 
-node_root.pre_order()
-print()
-node_root.post_order()
-print()
-node_root.in_order()
+array = [2, 3, 1, 4, 6, 5, 9, 8, 7]
+count = 0
 
+for i in range(len(array)):
+    for j in range(len(array) - i - 1):
+        count += 1
+        if array[j] > array[j + 1]:
+            array[j], array[j + 1] = array[j + 1], array[j]
 
+print(array)
+print(count)
