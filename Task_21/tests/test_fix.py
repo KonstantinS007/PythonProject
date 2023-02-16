f3ab04645a3244d159d31e571ccad323916b88dc
KonstantinS007 <uzerovalex9@gmail.com>
2023-02-17 02:46:37 +0300

    Task_21

--- Task_21/tests/test_fix.py
+++ Task_21/tests/test_fix.py
@@ -42,7 +42,49 @@ import pytest
 #     print(f"\nТест шел: {end_time - start_time}")
 
 
-@pytest.fixture(scope="class", autouse=True)
-def session_fixture():
-    print("\nclass fixture starts")
+# @pytest.fixture(scope="class", autouse=True) # 3
+# def session_fixture():
+#     print("\nclass fixture starts")
+#
+#
+# @pytest.fixture(scope="function", autouse=True) # 2
+# def function_fixture():
+#     print("\nfunction fixture starts")
 
+
+# @pytest.fixture(scope="session", autouse=True) # 1
+# def function_fixture():
+#     print("\nSession fixture starts")
+
+
+# class TestClass23:
+#
+#     def test_first(self):
+#         pass
+#
+#     def test_second(self):
+#         pass
+
+
+@pytest.fixture()
+def request_fixture(request):
+    print(request.fixturename)
+    print(request.scope)
+    print(request.function.__name__)
+    print(request.cls)
+    print(request.module.__name__)
+    print(request.fspath)
+    if request.cls:
+        return f"\n У теста {request.function.__name__} класс есть\n"
+    else:
+        return f"\n У теста {request.function.__name__} класса нет\n"
+
+
+def test_request_1(request_fixture):
+    print(request_fixture)
+
+
+class TestClassRequest:
+
+    def test_request_2(self, request_fixture):
+        print(request_fixture)
