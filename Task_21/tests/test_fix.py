47290646d81a427c3ba9d8d02e81a4bdca505a8e
KonstantinS007 <uzerovalex9@gmail.com>
2023-02-17 03:21:21 +0300

    Task_21

--- Task_21/tests/test_fix.py
+++ Task_21/tests/test_fix.py
@@ -74,6 +74,7 @@ def request_fixture(request):
     print(request.cls)
     print(request.module.__name__)
     print(request.fspath)
+    print(request.scope) # вспомнил африканца из ролика все очень просто фигвам деф
     if request.cls:
         return f"\n У теста {request.function.__name__} класс есть\n"
     else:
@@ -88,3 +89,4 @@ class TestClassRequest:
 
     def test_request_2(self, request_fixture):
         print(request_fixture)
+
