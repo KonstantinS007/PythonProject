a3a9ba8ed512b2847e76bc4fc1d0673c488b8018
KonstantinS007 <uzerovalex9@gmail.com>
2023-02-28 22:35:09 +0300

    Task_24 Selenium 4

--- Task_24/conftest.py
+++ Task_24/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-
+import uuid
 
 @pytest.fixture
 def firefox_options(firefox_options):
@@ -79,3 +79,40 @@ def pytest_collection_finish(session):
 
         pytest.exit('Done!')
 
+@pytest.hookimpl(hookwrapper=True, tryfirst=True)
+def pytest_runtest_makereport(item, call):
+    # This function helps to detect that some test failed
+    # and pass this information to teardown:
+
+    outcome = yield
+    rep = outcome.get_result()
+    setattr(item, "rep_" + rep.when, rep)
+    return rep
+
+@pytest.fixture
+def web_browser(request, selenium):
+
+    browser = selenium
+    browser.set_window_size(1400, 1000)
+
+    # Return browser instance to test case:
+    yield browser
+
+    # Do teardown (this code will be executed after each test):
+
+    if request.node.rep_call.failed:
+        # Make the screen-shot if test failed:
+        try:
+            browser.execute_script("document.body.bgColor = 'white';")
+
+            # Make screen-shot for local debug:
+            browser.save_screenshot('screenshots/' + str(uuid.uuid4()) + '.png')
+
+            # For happy debugging:
+            print('URL: ', browser.current_url)
+            print('Browser logs:')
+            for log in browser.get_log('browser'):
+                print(log)
+
+        except:
+            pass # just ignore any errors here
--- Task_24/tests/test_selenium_petfriends_pytest.py
+++ Task_24/tests/test_selenium_petfriends_pytest.py
@@ -14,43 +14,80 @@ import pickle
 #  3) Run tests:
 #     python -m pytest -v --driver Chrome --driver-path Task_24/chromedriver.exe test_selenium_petfriends_pytest.py
 #     python -m pytest -v --driver Chrome test_selenium_simple.py
-
+import codecs
 import time
 from selenium.webdriver.common.by import By
 
-def test_petfriends(selenium):
-    """ Search some phrase in google and make a screenshot of the page. """
+# def test_petfriends(selenium):
+#     """ Search some phrase in google and make a screenshot of the page. """
+#
+#     # Open PetFriends base page:
+#     selenium.get("https://petfriends.skillfactory.ru")
+#
+#     time.sleep(10)  # just for demo purposes, do NOT repeat it on real projects!
+#
+#     # Find the field for search text input:
+#     btn_newuser = selenium.find_element(By.XPATH, "//button[@onclick=\"document.location='/new_user';\"]")
+#
+#     btn_newuser.click()
+#
+#     btn_exist_acc = selenium.find_element(By.LINK_TEXT, u"У меня уже есть аккаунт")
+#     btn_exist_acc.click()
+#
+#     field_email = selenium.find_element(By.ID, "email")
+#     field_email.click()
+#     field_email.clear()
+#     field_email.send_keys("uzerovalex9@gmail.com")
+#
+#     field_pass = selenium.find_element(By.ID, "pass")
+#     field_pass.click()
+#     field_pass.clear()
+#     field_pass.send_keys("qwerty123")
+#
+#     btn_submit = selenium.find_element(By.XPATH, "//button[@type='submit']")
+#     btn_submit.click()
+#
+#     # Save cookies of the browser after the login
+#     with open('my_cookies.txt', 'wb') as cookies:
+#         pickle.dump(selenium.get_cookies(), cookies)
+#
+#     # Make the screenshot of browser window:
+#     if web_browser.current_url == 'https://petfriends.skillfactory.ru/all_pets':
+#         # Make the screenshot of browser window:
+#         selenium.save_screenshot('result_petfriends.jpg')
+#     else:
+#         raise Exception("login error")
 
-    # Open PetFriends base page:
-    selenium.get("https://petfriends.skillfactory.ru")
 
-    time.sleep(10)  # just for demo purposes, do NOT repeat it on real projects!
+def test_petfriends(web_browser):
+   # Open PetFriends base page:
+   web_browser.get("https://petfriends.skillfactory.ru/")
 
-    # Find the field for search text input:
-    btn_newuser = selenium.find_element(By.XPATH, "//button[@onclick=\"document.location='/new_user';\"]")
+   time.sleep(5)  # just for demo purposes, do NOT repeat it on real projects!
 
-    btn_newuser.click()
+   # click on the new user button
+   btn_newuser = web_browser.find_element(By.XPATH, "//button[@onclick=\"document.location='/new_user';\"]")
+   btn_newuser.click()
 
-    btn_exist_acc = selenium.find_element(By.LINK_TEXT, u"У меня уже есть аккаунт")
-    btn_exist_acc.click()
+   # click existing user button
+   btn_exist_acc = web_browser.find_element(By.LINK_TEXT, u"У меня уже есть аккаунт")
+   btn_exist_acc.click()
 
-    field_email = selenium.find_element(By.ID, "email")
-    field_email.click()
-    field_email.clear()
-    field_email.send_keys("uzerovalex9@gmail.com")
+   # add email
+   field_email = web_browser.find_element(By.ID, "email")
+   field_email.clear()
+   field_email.send_keys("uzerovalex9@gmail.com")
 
-    field_pass = selenium.find_element(By.ID, "pass")
-    field_pass.click()
-    field_pass.clear()
-    field_pass.send_keys("qwerty123")
+   # add password
+   field_pass = web_browser.find_element(By.ID, "pass")
+   field_pass.clear()
+   field_pass.send_keys("qwerty123")
 
-    btn_submit = selenium.find_element(By.XPATH, "//button[@type='submit']")
-    btn_submit.click()
+   # click submit button
+   btn_submit = web_browser.find_element(By.XPATH, "//button[@type='submit']")
+   btn_submit.click()
 
-    # Save cookies of the browser after the login
-    with open('my_cookies.txt', 'wb') as cookies:
-        pickle.dump(selenium.get_cookies(), cookies)
 
-    # Make the screenshot of browser window:
-    selenium.save_screenshot('result_petfriends.png')
+   time.sleep(5)  # just for demo purposes, do NOT repeat it on real projects!
 
+   assert  web_browser.current_url == 'https://petfriends.skillfactory.ru/all_pets',"login error"
